da89d8cd0b2d28c4d0103019aaf384204390f7cd
Mohammed Shalaby <mohammed.shalaby@mail.mcgill.ca>
2024-02-20 17:28:52 -0500

    Extract all topics from a given rosbag, into either jpegs or csvs

--- preprocess/read_bags.py
+++ preprocess/read_bags.py
@@ -5,6 +5,7 @@ import rospy
 import rosbag
 import cv2
 from cv_bridge import CvBridge
+from bagpy import bagreader
 
 def write_imgs(input_bag, dir_main):
     bridge = CvBridge()
@@ -18,23 +19,31 @@ def write_imgs(input_bag, dir_main):
                 dir = dir_main + "infra2/"
             elif "bottom" in topic:
                 dir = dir_main + "bottom/"
-            else:
-                continue
+            elif "color" in topic:
+                dir = dir_main + "color/"
             try:
                 cv_image = bridge.compressed_imgmsg_to_cv2(msg)
                 cv2.imwrite(dir + str(msg.header.stamp) + '.jpeg', cv_image)
             except Exception as e:
                 rospy.logwarn('Error uncompressing image: {}'.format(e))
-        else:
+
+def write_csvs(input_bag):
+    b = bagreader(input_bag)
+    for topic in b.topics:
+        if rospy.is_shutdown():
+            break
+        if "camera" in topic and "imu" not in topic:
             continue
+        else:
+            b.message_by_topic(topic)
 
 if __name__ == '__main__':
+    # TODO: Allow user-defined image compression type
     if len(sys.argv) != 2:
-        print("Not enough arguments. Usage: python bag_to_imgs.py input_bag")
+        print("Not enough arguments. Usage: python read_bags.py input_bag")
         sys.exit(1)
     
     path = sys.argv[1]
-    # output_bag = sys.argv[2]
 
     files = [f for f in listdir(path) if f.endswith('.bag')]
 
@@ -45,5 +54,10 @@ if __name__ == '__main__':
         mkdir(join(path, file.split(".")[0] + "/infra1"))
         mkdir(join(path, file.split(".")[0] + "/infra2"))
         mkdir(join(path, file.split(".")[0] + "/bottom"))
+        mkdir(join(path, file.split(".")[0] + "/color"))
+
         write_imgs(join(path, file), join(path, file.split(".")[0]) + "/")
+        write_csvs(join(path, file))
+
+        
     
