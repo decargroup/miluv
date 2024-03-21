d7616bcc60736bc94f673bbf9dcf51805800a04c
Mohammed Shalaby <mohammed.shalaby@mail.mcgill.ca>
2024-03-21 00:04:38 -0400

    Parallelize preprocessing

--- preprocess/preprocess.sh
+++ preprocess/preprocess.sh
@@ -1,8 +1,17 @@
+
+task(){
+    python preprocess/read_bags.py $d True
+    python preprocess/process_uwb.py $d
+    python preprocess/cleanup_csv.py $d
+}
+
+N=16
+(
 for d in data/*/ ; do
     if [ ${#d} -gt 9 ]; then
         continue
     fi
-    python preprocess/read_bags.py $d True
-    python preprocess/process_uwb.py $d
-    python preprocess/cleanup_csv.py $d
+    ((i=i%N)); ((i++==0)) && wait
+    task $d &
 done
+)
