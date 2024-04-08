6ad6c066e7e4903e23c1a9b78d5058b2c6eaa12a
Syed Shabbir Ahmed <syed.shabbir.ahmed@mail.mcgill.ca>
2024-04-08 13:10:04 -0400

    minor

--- src/filters.py
+++ src/filters.py
@@ -1,7 +1,7 @@
 """ 
 Extended Kalman Filter.
 """
-from utils.meas import Measurement
+from utils.models import Measurement
 from utils.states import StateWithCovariance
 import numpy as np
 from scipy.stats.distributions import chi2
