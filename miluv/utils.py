0ff6a0fb70c7837e7795055027f8f36eb969179b
Nicholas <nicholas.nfd@gmail.com>
2024-04-22 13:53:57 -0400

    Put type hinting in quotes to fix error on Python 3.8

--- miluv/utils.py
+++ miluv/utils.py
@@ -6,7 +6,8 @@ import scipy as sp
 import yaml
 import matplotlib.pyplot as plt
 
-def get_mocap_splines(mocap: pd.DataFrame) -> tuple[callable, callable]:
+
+def get_mocap_splines(mocap: pd.DataFrame) -> 'tuple[callable, callable]':
     """
     Get spline interpolations for mocap data.
     
@@ -17,39 +18,40 @@ def get_mocap_splines(mocap: pd.DataFrame) -> tuple[callable, callable]:
     - pos_splines: Spline interpolation for position.
     - quat_splines: Spline interpolation for orientation.
     """
-     
+
     # Get mocap data
     time = mocap['timestamp'].values
-    pos = mocap[
-        ["pose.position.x", "pose.position.y", "pose.position.z"]
-    ].values
-    quat = mocap[
-        ["pose.orientation.x", "pose.orientation.y", "pose.orientation.z", "pose.orientation.w"]
-    ].values
-    
+    pos = mocap[["pose.position.x", "pose.position.y",
+                 "pose.position.z"]].values
+    quat = mocap[[
+        "pose.orientation.x", "pose.orientation.y", "pose.orientation.z",
+        "pose.orientation.w"
+    ]].values
+
     # Remove mocap gaps
     pos_gaps = np.linalg.norm(pos, axis=1) < 1e-6
     quat_gaps = np.linalg.norm(quat, axis=1) < 1e-6
     gaps = pos_gaps | quat_gaps
-    
+
     time = time[~gaps]
     pos = pos[~gaps]
     quat = quat[~gaps]
-    
+
     # Normalize quaternion
     quat /= np.linalg.norm(quat, axis=1)[:, None]
-    
+
     # Resolve quaternion discontinuities
     for i in range(1, len(quat)):
-        if np.dot(quat[i], quat[i-1]) < 0:
+        if np.dot(quat[i], quat[i - 1]) < 0:
             quat[i] *= -1
-    
+
     # Fit splines
     pos_splines = csaps(time, pos.T, smooth=0.9999)
     quat_splines = csaps(time, quat.T, smooth=0.9999)
-    
+
     return pos_splines, quat_splines
-    
+
+
 def get_timeshift(exp_name):
     """
     Get timeshift.
@@ -60,13 +62,14 @@ def get_timeshift(exp_name):
     Returns:
     - timeshift: Timeshift in seconds.
     """
-    
+
     with open(f"data/{exp_name}/timeshift.yaml", "r") as file:
         timeshift = yaml.safe_load(file)
     timeshift_s = timeshift["timeshift_s"]
     timeshift_ns = timeshift["timeshift_ns"]
-    
+
     return timeshift_s + timeshift_ns / 1e9
+
 
 def load_vins(exp_name, robot_id, loop=True):
     """
@@ -80,42 +83,39 @@ def load_vins(exp_name, robot_id, loop=True):
     Returns:
     - vins: VINS data.
     """
-    
+
     if loop:
         file = f"data/vins/{exp_name}/{robot_id}_vio_loop.csv"
     else:
         file = f"data/vins/{exp_name}/{robot_id}_vio.csv"
-        
-    data = pd.read_csv(
-        file, 
-        names=[
-            "timestamp",
-            "pose.position.x",
-            "pose.position.y",
-            "pose.position.z",
-            "pose.orientation.x",
-            "pose.orientation.y",
-            "pose.orientation.z",
-            "pose.orientation.w",
-            "twist.linear.x",
-            "twist.linear.y",
-            "twist.linear.z",
-        ],
-        index_col=False
-    )
-    
+
+    data = pd.read_csv(file,
+                       names=[
+                           "timestamp",
+                           "pose.position.x",
+                           "pose.position.y",
+                           "pose.position.z",
+                           "pose.orientation.x",
+                           "pose.orientation.y",
+                           "pose.orientation.z",
+                           "pose.orientation.w",
+                           "twist.linear.x",
+                           "twist.linear.y",
+                           "twist.linear.z",
+                       ],
+                       index_col=False)
+
     timeshift = get_timeshift(exp_name)
-    data["timestamp"] = data["timestamp"]/1e9 - timeshift
-    
+    data["timestamp"] = data["timestamp"] / 1e9 - timeshift
+
     return data
 
-def save_vins(
-    data: pd.DataFrame,
-    exp_name: str, 
-    robot_id: str, 
-    loop: bool = True,
-    suffix: str = ""
-):
+
+def save_vins(data: pd.DataFrame,
+              exp_name: str,
+              robot_id: str,
+              loop: bool = True,
+              suffix: str = ""):
     """
     Save VINS data.
     
@@ -127,10 +127,13 @@ def save_vins(
     - suffix: Suffix to append to the csv file name.
     """
     if loop:
-        data.to_csv(f"data/vins/{exp_name}/{robot_id}_vio_loop{suffix}.csv", index=False)
+        data.to_csv(f"data/vins/{exp_name}/{robot_id}_vio_loop{suffix}.csv",
+                    index=False)
     else:
-        data.to_csv(f"data/vins/{exp_name}/{robot_id}_vio{suffix}.csv", index=False)
-    
+        data.to_csv(f"data/vins/{exp_name}/{robot_id}_vio{suffix}.csv",
+                    index=False)
+
+
 def align_frames(df1, df2):
     """
     Align inertial reference frames for two dataframes consisting of body-frame data. 
@@ -158,22 +161,26 @@ def align_frames(df1, df2):
     - r: Translation vector from mocap frame to VINS frame, resolved in the mocap frame.
     """
     pos1 = df1[[
-        "pose.position.x", "pose.position.y", "pose.position.z", 
+        "pose.position.x",
+        "pose.position.y",
+        "pose.position.z",
     ]].values
-    
+
     pos2 = df2[[
-        "pose.position.x", "pose.position.y", "pose.position.z",
+        "pose.position.x",
+        "pose.position.y",
+        "pose.position.z",
     ]].values
-    
+
     y = pos1
 
     C_hat = np.eye(3)
     r_hat = np.zeros(3)
-    
+
     # Levenberg-Marquardt optimization
     def error(y, C, r):
         return (y - (C @ (pos2 - r).T).T).flatten()
-    
+
     def jacobian(C, r):
         J = np.empty((0, 6))
         for pos in pos2:
@@ -182,7 +189,7 @@ def align_frames(df1, df2):
             J_iter[:, 3:] = -C
             J = np.vstack((J, J_iter))
         return J
-    
+
     del_x = np.ones(6)
     iter = 0
     e = error(y, C_hat, r_hat)
@@ -193,7 +200,7 @@ def align_frames(df1, df2):
         r_hat = r_hat + del_x[3:]
         C_hat = C_hat @ sp.linalg.expm(so3_wedge_matrix(del_x[:3]))
         iter += 1
-        
+
         e = error(y, C_hat, r_hat)
         print("Iteration: ", iter)
         print("Error: ", e)
@@ -201,18 +208,15 @@ def align_frames(df1, df2):
         print("Delta x: ", del_x)
         print("Delta x norm: ", np.linalg.norm(del_x))
         print("C_hat: ", C_hat)
-        print("r_hat: ", r_hat) 
+        print("r_hat: ", r_hat)
         print("-------------------")
-            
+
     # Apply transformation to df1
     df1 = apply_transformation(df1, C_hat, r_hat)
-    
-    return {
-        "data": df1,
-        "C": C_hat,
-        "r": r_hat
-    }
-    
+
+    return {"data": df1, "C": C_hat, "r": r_hat}
+
+
 def apply_transformation(df, C, r):
     """
     Apply a transformation to a dataframe consisting of body-frame data.
@@ -225,34 +229,41 @@ def apply_transformation(df, C, r):
     Returns:
     - df: Dataframe with transformed data.
     """
-    
+
     pose = df[[
         "pose.position.x", "pose.position.y", "pose.position.z",
-        "pose.orientation.x", "pose.orientation.y", "pose.orientation.z", "pose.orientation.w"
+        "pose.orientation.x", "pose.orientation.y", "pose.orientation.z",
+        "pose.orientation.w"
     ]].values
 
     df_r = pose[:, :3]
     df_quat = pose[:, 3:]
-    df_C = np.array([Rotation.from_quat(df_quat[i]).as_matrix() for i in range(len(df_quat))])
+    df_C = np.array([
+        Rotation.from_quat(df_quat[i]).as_matrix() for i in range(len(df_quat))
+    ])
 
     pose = np.array([C.T @ df_r[i] + r for i in range(len(df_r))])
     df[[
-        "pose.position.x", "pose.position.y", "pose.position.z",
-    ]] = np.array([pose[i] for i in range(len(pose))])    
+        "pose.position.x",
+        "pose.position.y",
+        "pose.position.z",
+    ]] = np.array([pose[i] for i in range(len(pose))])
     df[[
-        "pose.orientation.x", "pose.orientation.y", "pose.orientation.z", "pose.orientation.w"
-    ]] = np.array([Rotation.from_matrix(df_C[i].T @ C).as_quat() for i in range(len(df_C))])
-    
+        "pose.orientation.x", "pose.orientation.y", "pose.orientation.z",
+        "pose.orientation.w"
+    ]] = np.array([
+        Rotation.from_matrix(df_C[i].T @ C).as_quat() for i in range(len(df_C))
+    ])
+
     if "twist.linear.x" in df.columns:
-        df_vel = df[[
-            "twist.linear.x", "twist.linear.y", "twist.linear.z"
-        ]].values
+        df_vel = df[["twist.linear.x", "twist.linear.y",
+                     "twist.linear.z"]].values
         vel = np.array([C.T @ df_vel[i] for i in range(len(df_vel))])
-        df[[
-            "twist.linear.x", "twist.linear.y", "twist.linear.z"
-        ]] = np.array([vel[i] for i in range(len(vel))])
-    
+        df[["twist.linear.x", "twist.linear.y",
+            "twist.linear.z"]] = np.array([vel[i] for i in range(len(vel))])
+
     return df
+
 
 def so3_wedge_matrix(omega):
     """
@@ -264,7 +275,7 @@ def so3_wedge_matrix(omega):
     Returns:
     - omega_hat: 3x3 SO(3) cross matrix.
     """
-    
+
     omega_hat = np.zeros((3, 3))
     omega_hat[0, 1] = -omega[2]
     omega_hat[0, 2] = omega[1]
@@ -272,8 +283,9 @@ def so3_wedge_matrix(omega):
     omega_hat[1, 2] = -omega[0]
     omega_hat[2, 0] = -omega[1]
     omega_hat[2, 1] = omega[0]
-    
+
     return omega_hat
+
 
 def compute_position_rmse(df1, df2):
     """
@@ -287,13 +299,11 @@ def compute_position_rmse(df1, df2):
     Returns:
     - rmse: RMSE.
     """
-    
-    pos1 = df1[[
-        "pose.position.x", "pose.position.y", "pose.position.z"
-    ]].values
-    
-    pos2 = df2[[
-        "pose.position.x", "pose.position.y", "pose.position.z"
-    ]].values
-    
+
+    pos1 = df1[["pose.position.x", "pose.position.y",
+                "pose.position.z"]].values
+
+    pos2 = df2[["pose.position.x", "pose.position.y",
+                "pose.position.z"]].values
+
     return np.sqrt(np.mean(np.linalg.norm(pos1 - pos2, axis=1)**2))
