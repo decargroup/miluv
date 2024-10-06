b0600fd73733cace3da626722a1044131ae06f0c
Mohammed Shalaby <mohammed.shalaby@mail.mcgill.ca>
2024-10-06 19:58:46 -0400

    Implement EKF for one robot vins case

--- examples/ekf_vins_one.py
+++ examples/ekf_vins_one.py
@@ -1,15 +1,36 @@
 # %%
 from miluv.data import DataLoader
-# from miluv.utils import load_vins, zero_order_hold
 import utils.liegroups as liegroups
 import miluv.utils as utils
+import examples.ekfutils.vins_one as vins_one
 
 import numpy as np
+import pandas as pd
 
 #################### LOAD SENSOR DATA ####################
 miluv = DataLoader("13", imu = "px4", cam = None, mag = False)
 data = miluv.data["ifo001"]
 vins = utils.load_vins("13", "ifo001", loop = False, postprocessed = True)
+
+#################### GET UWB TRANSCEIVER POSITIONS ####################
+# anchor_positions = utils.get_anchors()
+# tag_moment_arms = utils.get_tag_moment_arms()
+anchor_positions = {
+    0: np.array([3.273827392578125, 3.46404736328125, 1.8093309326171875]).reshape(3, 1),
+    1: np.array([3.186386962890625, 0.27394485473632812, 1.5884853515625]).reshape(3, 1),
+    2: np.array([2.850500244140625, -2.923056884765625, 1.89742041015625]).reshape(3, 1),
+    3: np.array([-2.497634521484375, -3.5018203125, 1.7730911865234375]).reshape(3, 1),
+    4: np.array([-2.95793310546875, 0.6128419189453125, 1.65714208984375]).reshape(3, 1),
+    5: np.array([-2.734676513671875, 3.65854248046875, 1.890254638671875]).reshape(3, 1),
+}
+tag_moment_arms = {
+    10: np.array([0.13189,-0.17245,-0.05249]).reshape(3, 1),
+    11: np.array([-0.17542,0.15712,-0.05307]).reshape(3, 1),
+    20: np.array([0.16544,-0.15085,-0.03456]).reshape(3, 1),
+    21: np.array([-0.15467,0.16972,-0.01680]).reshape(3, 1),
+    30: np.array([0.16685,-0.18113,-0.05576]).reshape(3, 1),
+    31: np.array([-0.13485,0.15468,-0.05164]).reshape(3, 1),
+}
 
 #################### ALIGN SENSOR DATA TIMESTAMPS ####################
 # TODO DOCUMENTATION: Timestamps where range measurements or height are available, assume vins measurements are constant between these timestamps
@@ -23,7 +44,7 @@ query_timestamps = query_timestamps[query_timestamps < vins["timestamp"].iloc[-1
 query_timestamps = np.sort(np.unique(query_timestamps))
 
 imu_at_query_timestamps = miluv.query_by_timestamps(query_timestamps, robots="ifo001", sensors="imu_px4")["ifo001"]
-gyro = imu_at_query_timestamps["imu_px4"][["timestamp", "angular_velocity.x", "angular_velocity.y", "angular_velocity.z"]]
+gyro: pd.DataFrame = imu_at_query_timestamps["imu_px4"][["timestamp", "angular_velocity.x", "angular_velocity.y", "angular_velocity.z"]]
 vins = utils.zero_order_hold(query_timestamps, vins) # add this and use it in by_timestamp inside the dataloader
 
 #################### LOAD GROUND TRUTH DATA ####################
@@ -33,11 +54,37 @@ gt_se3 = liegroups.get_se3_poses(
 )
 
 #################### EKF ####################
-# ekf = EKF("vins_one")
-# ekf.initialize(state)
-# for sensor data
-#     ekf.predict()
-#     ekf.correct()
+ekf = vins_one.EKF(gt_se3[0], anchor_positions, tag_moment_arms)
+for i in range(0, len(query_timestamps)):
+    input = np.array([
+        gyro.iloc[i]["angular_velocity.x"], 
+        gyro.iloc[i]["angular_velocity.y"], 
+        gyro.iloc[i]["angular_velocity.z"],
+        vins.iloc[i]["twist.linear.x"],
+        vins.iloc[i]["twist.linear.y"],
+        vins.iloc[i]["twist.linear.z"],
+    ])
+    
+    dt = (query_timestamps[i] - query_timestamps[i - 1]) if i > 0 else 0
+    ekf.predict(input, dt)
+    
+    range_idx = np.where(data["uwb_range"]["timestamp"] == query_timestamps[i])[0]
+    if len(range_idx) > 0:
+        range_data = data["uwb_range"].iloc[range_idx]
+        ekf.correct({
+            "range": float(range_data["range"].iloc[0]),
+            "to_id": int(range_data["to_id"].iloc[0]),
+            "from_id": int(range_data["from_id"].iloc[0])
+        })
+        
+    height_idx = np.where(data["height"]["timestamp"] == query_timestamps[i])[0]
+    if len(height_idx) > 0:
+        height_data = data["height"].iloc[height_idx]
+        ekf.correct({
+            "height": float(height_data["range"].iloc[0])
+        })
+        
+    print(ekf.x)
 
 #################### POSTPROCESS ####################
 
---
+++ examples/ekfutils/vins_one.py
@@ -0,0 +1,81 @@
+import numpy as np
+from pymlg import SE3
+
+state_dimension = 6
+
+P0 = np.eye(6)
+Q = np.eye(6) / 1e3
+R_range = 0.1
+R_height = 0.1
+
+class EKF:
+    def __init__(self, state, anchors, tag_moment_arms):
+        self.x = state #TODO: add noise
+        self.P = P0
+        self.anchors = anchors
+        self.tag_moment_arms = tag_moment_arms
+
+    def predict(self, u, dt):
+        A = self._process_jacobian(self.x, u, dt)
+        Qd = self._process_covariance(self.x, u, dt)
+        
+        self.x = self._process_model(self.x, u, dt)
+        self.P = A @ self.P @ A.T + Qd
+    
+    def correct(self, y):
+        if "range" in y:
+            anchor_id = y["to_id"]
+            tag_id = y["from_id"]
+            H = self._range_jacobian(self.x, anchor_id, tag_id)
+            R = R_range
+            actual_measurement = y["range"]
+            predicted_measurement = self._range_measurement(self.x, anchor_id, tag_id)
+        elif "height" in y:
+            H = self._height_jacobian(self.x)
+            R = R_height
+            actual_measurement = y["height"]
+            predicted_measurement = self._height_measurement(self.x)
+        else:
+            return
+        
+        K = self.P @ H.T / (H @ self.P @ H.T + R)
+        
+        self.x = self.x @ SE3.Exp(K @ np.array([actual_measurement - predicted_measurement]))
+        self.P = (np.eye(6) - K @ H) @ self.P
+        self.P = 0.5 * (self.P + self.P.T)
+        
+    @staticmethod
+    def _process_model(x, u, dt):
+        return x @ SE3.Exp(u * dt)
+    
+    @staticmethod
+    def _process_jacobian(x, u, dt):
+        return SE3.adjoint(np.linalg.inv(SE3.Exp(u * dt)))
+    
+    @staticmethod
+    def _process_covariance(x, u, dt):
+        return dt * SE3.left_jacobian(-dt * u) @ Q @ SE3.left_jacobian(-dt * u).T
+    
+    def _range_measurement(self, x, anchor_id, tag_id):
+        Pi = np.hstack([np.eye(3), np.zeros((3, 1))])
+        r_tilde = np.vstack((self.tag_moment_arms[tag_id], 1)).reshape(4, 1)
+        
+        return np.linalg.norm(self.anchors[anchor_id] - Pi @ x @ r_tilde)
+    
+    def _range_jacobian(self, x, anchor_id, tag_id):
+        Pi = np.hstack([np.eye(3), np.zeros((3, 1))])
+        r_tilde = np.vstack((self.tag_moment_arms[tag_id], 1)).reshape(4, 1)
+        
+        vector = (self.anchors[anchor_id] - Pi @ x @ r_tilde).reshape(3, 1)
+        
+        return -1 * vector.T / np.linalg.norm(vector) @ Pi @ x @ SE3.odot(r_tilde)
+    
+    @staticmethod
+    def _height_measurement(x):
+        return x[2, 3]
+    
+    @staticmethod
+    def _height_jacobian(x):
+        a = np.array([0, 0, 1, 0]).reshape(4, 1)
+        b = np.array([0, 0, 0, 1])
+        return (a.T @ x @ SE3.odot(b))
