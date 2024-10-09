59af0e52a81b512946169e6df8ded464d487e33b
Mohammed Shalaby <mohammed.shalaby@mail.mcgill.ca>
2024-10-08 21:00:11 -0400

    Add postprocessing and plotting

--- examples/ekf_vins_one.py
+++ examples/ekf_vins_one.py
@@ -3,14 +3,18 @@ from miluv.data import DataLoader
 import utils.liegroups as liegroups
 import miluv.utils as utils
 import examples.ekfutils.vins_one as vins_one
+import examples.ekfutils.postprocessing as postprocessing
 
 import numpy as np
 import pandas as pd
 
+#################### EXPERIMENT DETAILS ####################
+exp_name = "13"
+
 #################### LOAD SENSOR DATA ####################
-miluv = DataLoader("13", imu = "px4", cam = None, mag = False)
+miluv = DataLoader(exp_name, imu = "px4", cam = None, mag = False)
 data = miluv.data["ifo001"]
-vins = utils.load_vins("13", "ifo001", loop = False, postprocessed = True)
+vins = utils.load_vins(exp_name, "ifo001", loop = False, postprocessed = True)
 
 #################### ALIGN SENSOR DATA TIMESTAMPS ####################
 # TODO DOCUMENTATION: Timestamps where range measurements or height are available, assume vins measurements are constant between these timestamps
@@ -24,7 +28,7 @@ query_timestamps = np.sort(np.unique(query_timestamps))
 
 imu_at_query_timestamps = miluv.query_by_timestamps(query_timestamps, robots="ifo001", sensors="imu_px4")["ifo001"]
 gyro: pd.DataFrame = imu_at_query_timestamps["imu_px4"][["timestamp", "angular_velocity.x", "angular_velocity.y", "angular_velocity.z"]]
-vins = utils.zero_order_hold(query_timestamps, vins) # add this and use it in by_timestamp inside the dataloader
+vins = utils.zero_order_hold(query_timestamps, vins)
 
 #################### LOAD GROUND TRUTH DATA ####################
 gt_se3 = liegroups.get_se3_poses(
@@ -32,17 +36,26 @@ gt_se3 = liegroups.get_se3_poses(
 )
 
 #################### EKF ####################
+# Initialize a variable to store the EKF state and covariance at each query timestamp for postprocessing
+ekf_history = postprocessing.History()
+
+# Initialize the EKF with the first ground truth pose, the anchor postions, and UWB tag moment arms
 ekf = vins_one.EKF(gt_se3[0], miluv.anchors, miluv.tag_moment_arms)
+
+# Iterate through the query timestamps
 for i in range(0, len(query_timestamps)):
+    # Get the gyro and vins data at this query timestamp for the EKF input
     input = np.array([
         gyro.iloc[i]["angular_velocity.x"], gyro.iloc[i]["angular_velocity.y"], 
         gyro.iloc[i]["angular_velocity.z"], vins.iloc[i]["twist.linear.x"],
         vins.iloc[i]["twist.linear.y"], vins.iloc[i]["twist.linear.z"],
     ])
     
+    # Do an EKF prediction using the gyro and vins data
     dt = (query_timestamps[i] - query_timestamps[i - 1]) if i > 0 else 0
     ekf.predict(input, dt)
     
+    # Check if range data is available at this query timestamp, and do an EKF correction
     range_idx = np.where(data["uwb_range"]["timestamp"] == query_timestamps[i])[0]
     if len(range_idx) > 0:
         range_data = data["uwb_range"].iloc[range_idx]
@@ -52,13 +65,20 @@ for i in range(0, len(query_timestamps)):
             "from_id": int(range_data["from_id"].iloc[0])
         })
         
+    # Check if height data is available at this query timestamp, and do an EKF correction
     height_idx = np.where(data["height"]["timestamp"] == query_timestamps[i])[0]
     if len(height_idx) > 0:
         height_data = data["height"].iloc[height_idx]
         ekf.correct({"height": float(height_data["range"].iloc[0])})
         
-    print(ekf.x)
+    ekf_history.add(query_timestamps[i], ekf.x, ekf.P)
 
 #################### POSTPROCESS ####################
+analysis = postprocessing.Evaluate(gt_se3, ekf_history, exp_name)
+
+# TODO: Mention in the documentation that we need to create the results/plots directory
+analysis.plot_error()
+analysis.plot_poses()
+analysis.save_results()
 
 # %%
---
+++ examples/ekfutils/postprocessing.py
@@ -0,0 +1,86 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+from pymlg import SE3
+
+class History:
+    def __init__(self):
+        self.timestamps = np.empty(0)
+        self.states = np.empty((0, 4, 4))
+        self.covariances = np.empty((0, 6, 6))
+
+    def add(self, timestamp: float, state: np.ndarray, covariance: np.ndarray):
+        self.timestamps = np.append(self.timestamps, timestamp)
+        self.states = np.append(self.states, state.reshape(1, 4, 4), axis=0)
+        self.covariances = np.append(self.covariances, covariance.reshape(1, 6, 6), axis=0)
+
+    def get(self):
+        return self.timestamps, self.states, self.covariances
+    
+class Evaluate:
+    def __init__(self, gt_se3, ekf_history, exp_name):
+        self.timestamps, self.states, self.covariances = ekf_history.get()
+        self.gt_se3 = gt_se3
+        self.exp_name = exp_name
+        
+        self.error = np.zeros((len(self.gt_se3), 6))
+        for i in range(0, len(self.gt_se3)):
+            self.error[i, :] = SE3.Log(SE3.inverse(self.gt_se3[i]) @ self.states[i]).ravel()
+            
+        self.error_titles = [r"$\delta_{\theta_x}$", r"$\delta_{\theta_y}$", r"$\delta_{\theta_z}$", 
+                             r"$\delta_{x}$", r"$\delta_{y}$", r"$\delta_{z}$"]
+
+    def plot_poses(self):
+        fig, axs = plt.subplots(3, 2, figsize=(10, 10))
+        fig.suptitle("Ground Truth vs. EKF Poses")
+        
+        gt = np.array([SE3.Log(pose).ravel() for pose in self.gt_se3])
+        est = np.array([SE3.Log(pose).ravel() for pose in self.states])
+        for i in range(0, 6):
+            axs[i % 3, int(i > 2)].plot(self.timestamps, gt[:, i], label="GT")
+            axs[i % 3, int(i > 2)].plot(self.timestamps, est[:, i], label="Est")
+            axs[i % 3, int(i > 2)].set_ylabel(self.error_titles[i])
+        axs[2, 0].set_xlabel("Time [s]")
+        axs[2, 1].set_xlabel("Time [s]")
+        axs[0, 0].legend()
+        plt.savefig(f"results/plots/ekf_vins_one/{self.exp_name}_poses.pdf")
+        plt.close()
+
+    def plot_error(self):
+        fig, axs = plt.subplots(3, 2, figsize=(10, 10))
+        fig.suptitle("Three-Sigma Error Plots")
+        
+        for i in range(0, 6):
+            axs[i % 3, int(i > 2)].plot(self.timestamps, self.error[:, i])
+            axs[i % 3, int(i > 2)].fill_between(
+                self.timestamps,
+                -1 * 3*np.sqrt(self.covariances[:, i, i]), 
+                3*np.sqrt(self.covariances[:, i, i]), 
+                alpha=0.5
+            )
+            axs[i % 3, int(i > 2)].set_ylabel(self.error_titles[i])
+        axs[2, 0].set_xlabel("Time [s]")
+        axs[2, 1].set_xlabel("Time [s]")
+        plt.savefig(f"results/plots/ekf_vins_one/{self.exp_name}_error.pdf")
+        plt.close()
+
+    def save_results(self):
+        pos_rmse, att_rmse = self.get_rmse()
+        
+        myCsvRow = f"{self.exp_name},{pos_rmse},{att_rmse}\n"
+        
+        if not os.path.exists('results/ekf_vins_one.csv'):
+            with open('results/ekf_vins_one.csv','w') as file:
+                file.write("exp_name,pos_rmse,att_rmse\n")
+        
+        with open('results/ekf_vins_one.csv','a') as file:
+            file.write(myCsvRow)
+                
+    def get_rmse(self):
+        pos_rmse = np.sqrt(np.mean(self.error[:, 3:] ** 2))
+        att_rmse = np.sqrt(np.mean(self.error[:, :3] ** 2))
+        return pos_rmse, att_rmse
+        
+        
+        
