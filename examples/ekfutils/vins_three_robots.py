29aa6b9e2de94e4778edcbeae24e8cf3857024e5
Mohammed Shalaby <mohammed.shalaby@mail.mcgill.ca>
2024-10-21 19:51:44 -0400

    Add 3-robot evaluation

--- examples/ekfutils/vins_three_robots.py
+++ examples/ekfutils/vins_three_robots.py
@@ -176,72 +176,83 @@ class EKF:
         return jac
 
 class EvaluateEKF:
-    # TODO:   
-    def __init__(self, gt_se3: dict[str: SE3], ekf_history: common.MatrixStateHistory, exp_name: str):
-        self.timestamps, self.states, self.covariances = ekf_history.get()
-        self.gt_se3 = gt_se3
-        self.exp_name = exp_name
+    def __init__(self, gt_se3: dict[str: SE3], ekf_history: dict[str: common.MatrixStateHistory], exp_name: str):        
+        self.timestamps = {}
+        self.states = {}
+        self.covariances = {}
+        self.gt_se3 = {}
+        self.error = {}
         
-        self.error = np.zeros((len(self.gt_se3), 6))
-        for i in range(0, len(self.gt_se3)):
-            self.error[i, :] = SE3.Log(SE3.inverse(self.gt_se3[i]) @ self.states[i]).ravel()
+        for robot in robot_names:
+            self.timestamps[robot], self.states[robot], self.covariances[robot] = ekf_history[robot].get()
+            self.gt_se3[robot] = gt_se3[robot]
             
+            self.error[robot] = np.zeros((len(self.gt_se3[robot]), 6))
+            for i in range(0, len(self.gt_se3[robot])):
+                self.error[robot][i, :] = SE3.Log(SE3.inverse(self.gt_se3[robot][i]) @ self.states[robot][i]).ravel()
+
+        self.exp_name = exp_name
         self.error_titles = [r"$\delta_{\phi_x}$", r"$\delta_{\phi_y}$", r"$\delta_{\phi_z}$", 
                              r"$\delta_{x}$", r"$\delta_{y}$", r"$\delta_{z}$"]
 
     def plot_poses(self) -> None:
-        fig, axs = plt.subplots(3, 2, figsize=(10, 10))
-        fig.suptitle("Ground Truth vs. EKF Poses")
-        
-        gt = np.array([SE3.Log(pose).ravel() for pose in self.gt_se3])
-        est = np.array([SE3.Log(pose).ravel() for pose in self.states])
-        for i in range(0, 6):
-            axs[i % 3, int(i > 2)].plot(self.timestamps, gt[:, i], label="GT")
-            axs[i % 3, int(i > 2)].plot(self.timestamps, est[:, i], label="Est")
-            axs[i % 3, int(i > 2)].set_ylabel(self.error_titles[i])
-        axs[2, 0].set_xlabel("Time [s]")
-        axs[2, 1].set_xlabel("Time [s]")
-        axs[0, 0].legend()
-        
-        if not os.path.exists('results/plots/ekf_vins_one_robot'):
-            os.makedirs('results/plots/ekf_vins_one_robot')
-        plt.savefig(f"results/plots/ekf_vins_one_robot/{self.exp_name}_poses.pdf")
-        plt.close()
+        for robot in robot_names:
+            fig, axs = plt.subplots(3, 2, figsize=(10, 10))
+            fig.suptitle("Ground Truth vs. EKF Poses for " + robot)
+            
+            gt = np.array([SE3.Log(pose).ravel() for pose in self.gt_se3[robot]])
+            est = np.array([SE3.Log(pose).ravel() for pose in self.states[robot]])
+            for i in range(0, 6):
+                axs[i % 3, int(i > 2)].plot(self.timestamps[robot], gt[:, i], label="GT")
+                axs[i % 3, int(i > 2)].plot(self.timestamps[robot], est[:, i], label="Est")
+                axs[i % 3, int(i > 2)].set_ylabel(self.error_titles[i])
+            axs[2, 0].set_xlabel("Time [s]")
+            axs[2, 1].set_xlabel("Time [s]")
+            axs[0, 0].legend()
+            
+            if not os.path.exists('results/plots/ekf_vins_three_robots'):
+                os.makedirs('results/plots/ekf_vins_three_robots')
+            plt.savefig(f"results/plots/ekf_vins_three_robots/{self.exp_name}_poses_{robot}.pdf")
+            plt.close()
 
     def plot_error(self) -> None:
-        fig, axs = plt.subplots(3, 2, figsize=(10, 10))
-        fig.suptitle("Three-Sigma Error Plots")
-        
-        for i in range(0, 6):
-            axs[i % 3, int(i > 2)].plot(self.timestamps, self.error[:, i])
-            axs[i % 3, int(i > 2)].fill_between(
-                self.timestamps,
-                -1 * 3*np.sqrt(self.covariances[:, i, i]), 
-                3*np.sqrt(self.covariances[:, i, i]), 
-                alpha=0.5
-            )
-            axs[i % 3, int(i > 2)].set_ylabel(self.error_titles[i])
-        axs[2, 0].set_xlabel("Time [s]")
-        axs[2, 1].set_xlabel("Time [s]")
-        
-        if not os.path.exists('results/plots/ekf_vins_one_robot'):
-            os.makedirs('results/plots/ekf_vins_one_robot')
-        plt.savefig(f"results/plots/ekf_vins_one_robot/{self.exp_name}_error.pdf")
-        plt.close()
+        for robot in robot_names:
+            fig, axs = plt.subplots(3, 2, figsize=(10, 10))
+            fig.suptitle("Three-Sigma Error Plots for " + robot)
+            
+            for i in range(0, 6):
+                axs[i % 3, int(i > 2)].plot(self.timestamps[robot], self.error[robot][:, i])
+                axs[i % 3, int(i > 2)].fill_between(
+                    self.timestamps[robot],
+                    -1 * 3*np.sqrt(self.covariances[robot][:, i, i]), 
+                    3*np.sqrt(self.covariances[robot][:, i, i]), 
+                    alpha=0.5
+                )
+                axs[i % 3, int(i > 2)].set_ylabel(self.error_titles[i])
+            axs[2, 0].set_xlabel("Time [s]")
+            axs[2, 1].set_xlabel("Time [s]")
+            
+            if not os.path.exists('results/plots/ekf_vins_three_robots'):
+                os.makedirs('results/plots/ekf_vins_three_robots')
+            plt.savefig(f"results/plots/ekf_vins_three_robots/{self.exp_name}_error_{robot}.pdf")
+            plt.close()
 
     def save_results(self) -> None:
-        pos_rmse, att_rmse = self.get_rmse()
+        pos_rmse = {}
+        att_rmse = {}
+        for robot in robot_names:        
+            pos_rmse[robot], att_rmse[robot] = self.get_rmse(robot)
         
-        myCsvRow = f"{self.exp_name},{pos_rmse},{att_rmse}\n"
+        myCsvRow = f"{self.exp_name}," + ",".join([f"{pos_rmse[robot]},{att_rmse[robot]}" for robot in robot_names]) + "\n"
         
-        if not os.path.exists('results/ekf_vins_one_robot.csv'):
-            with open('results/ekf_vins_one_robot.csv','w') as file:
-                file.write("exp_name,pos_rmse,att_rmse\n")
+        if not os.path.exists('results/ekf_vins_three_robots.csv'):
+            with open('results/ekf_vins_three_robots.csv','w') as file:
+                file.write("exp_name," + ",".join([f"{robot}_pos_rmse,{robot}_att_rmse" for robot in robot_names]) + "\n")
         
-        with open('results/ekf_vins_one_robot.csv','a') as file:
+        with open('results/ekf_vins_three_robots.csv','a') as file:
             file.write(myCsvRow)
                 
-    def get_rmse(self) -> tuple[float, float]:
-        pos_rmse = np.sqrt(np.mean(self.error[:, 3:] ** 2))
-        att_rmse = np.sqrt(np.mean(self.error[:, :3] ** 2))
+    def get_rmse(self, robot: str) -> tuple[float, float]:
+        pos_rmse = np.sqrt(np.mean(self.error[robot][:, 3:] ** 2))
+        att_rmse = np.sqrt(np.mean(self.error[robot][:, :3] ** 2))
         return pos_rmse, att_rmse
