5cdb32aa43156f502d69423fe6951a7f8c6b2601
Mohammed Shalaby <mohammed.shalaby@mail.mcgill.ca>
2024-11-27 23:06:04 -0500

    Check if VINS directory exists before running VINS

--- preprocess/run_vins.sh
+++ preprocess/run_vins.sh
@@ -10,11 +10,14 @@ fi
 exp_name=$1
 robot=$2
 
+if [ ! -d "$(rospack find miluv)/data/vins" ]; then
+    mkdir -p $(rospack find miluv)/data/vins
+fi
+
 # Run VINS with a 4 minute timeout
 bag=$(rospack find miluv)/data/${exp_name}/${robot}.bag
-timeout 240 roslaunch miluv vins.launch robot:=$robot bag:=$bag > /dev/null 2>&1
+timeout 5 roslaunch miluv vins.launch robot:=$robot bag:=$bag > /dev/null 2>&1
 
-# Rename the output file 
 cd $(rospack find miluv)/data/vins
 mkdir -p $exp_name
 mv vio.csv $exp_name/${robot}_vio.csv
