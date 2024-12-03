ce8370cb5c94448fde1772560c2c3b1471b947ee
Mohammed Shalaby <mohammed.shalaby@mail.mcgill.ca>
2024-12-02 21:45:22 -0500

    Change back the vins timeout to be 4 minutes

--- preprocess/run_vins.sh
+++ preprocess/run_vins.sh
@@ -16,7 +16,7 @@ fi
 
 # Run VINS with a 4 minute timeout
 bag=$(rospack find miluv)/data/${exp_name}/${robot}.bag
-timeout 5 roslaunch miluv vins.launch robot:=$robot bag:=$bag > /dev/null 2>&1
+timeout 240 roslaunch miluv vins.launch robot:=$robot bag:=$bag > /dev/null 2>&1
 
 cd $(rospack find miluv)/data/vins
 mkdir -p $exp_name
